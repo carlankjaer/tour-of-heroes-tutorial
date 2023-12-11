fb935db9bb3f7327fcbac836fcbb909bbd00359b
Caroline <carlankjaer@gmail.com>
2023-12-11 13:16:58 +0100

    Add search function to dashboard and get data from server

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { HeroesComponent } from "./heroes/heroes.component";
+import { HeroesComponent } from './heroes/heroes.component';
 import { MessagesComponent } from './messages/messages.component';
 
 @Component({
-    selector: 'app-root',
-    standalone: true,
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.css',
-    imports: [
-      CommonModule, 
-      RouterModule, 
-      HeroesComponent, 
-      MessagesComponent,
-    ]
+  selector: 'app-root',
+  standalone: true,
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+  imports: [CommonModule, RouterModule, HeroesComponent, MessagesComponent],
 })
 export class AppComponent {
   title = 'Tour of Heroes';
